14a2237a44edb8a4fb48c566440e92bcb1b57b2b
Raheem Adebayo <adraheemzy@gmail.com>
2023-02-20 23:51:20 +0100

    save

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,7 +5,9 @@ const Form = () => {
   const [fullName, setFullName] = useState("");
   const [age, setAge] = useState("0");
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
     const obj = {
       name: fullName,
       age: age,
@@ -21,36 +23,38 @@ const Form = () => {
       <h1>My Form</h1>
 
       <div>
-        <input
-          type="text"
-          placeholder="Enter your name"
-          style={{
-            width: 300,
-            padding: 20,
-            fontSize: "1.5rem",
-            marginRight: 20,
-          }}
-          onChange={(e) => {
-            const value = e.target.value;
-            setFullName(value);
-          }}
-          value={fullName}
-        />
+        <form>
+          <input
+            type="text"
+            placeholder="Enter your name"
+            style={{
+              width: 300,
+              padding: 20,
+              fontSize: "1.5rem",
+              marginRight: 20,
+            }}
+            onChange={(e) => {
+              const value = e.target.value;
+              setFullName(value);
+            }}
+            value={fullName}
+          />
 
-        <input
-          type="number"
-          placeholder="Enter your age"
-          style={{ width: 300, padding: 20, fontSize: "1.5rem" }}
-          onChange={(e) => {
-            const value = e.target.value;
-            setAge(value);
-          }}
-          value={age}
-        />
+          <input
+            type="number"
+            placeholder="Enter your age"
+            style={{ width: 300, padding: 20, fontSize: "1.5rem" }}
+            onChange={(e) => {
+              const value = e.target.value;
+              setAge(value);
+            }}
+            value={age}
+          />
 
-        <button onClick={handleSubmit}>Submit</button>
+          <button onClick={handleSubmit}>Submit</button>
 
-        <Greet name={fullName} age={age} />
+          <Greet name={fullName} age={age} />
+        </form>
       </div>
     </div>
   );
